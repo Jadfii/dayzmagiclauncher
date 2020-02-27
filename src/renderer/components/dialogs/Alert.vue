bf6a013e5e4156d76fc45f95856e98701e2f0d3a
Jadfii <Jadfii@users.noreply.github.com>
2020-02-27 13:07:41 +0000

    Changed alert model to parse message as html

--- src/renderer/components/dialogs/Alert.vue
+++ src/renderer/components/dialogs/Alert.vue
@@ -3,13 +3,12 @@
         <div v-if="show" class="position-fixed d-flex align-items-center justify-content-center" style="z-index: 2; top: 0; left: 0; right: 0; bottom: 0; width: 100vw; height: 100vh;">
             <div class="modal-backdrop show"></div>
             <div class="modal modal-xl position-relative d-flex">
-                <div class="modal-dialog modal-dialog-centered" style="width: 33% !important;">
+                <div class="modal-dialog modal-dialog-centered" style="width: 66% !important;">
                     <div class="modal-content border-0 bg-4">
                         <div class="modal-header d-flex flex-column">
                             <h5 class="modal-title">{{ title }}</h5>
                         </div>
-                        <div class="modal-body d-flex flex-column">
-                            {{ message }}
+                        <div class="modal-body d-flex flex-column" v-html="message">
                         </div>
                         <div class="modal-footer">
                             <button type="button" class="btn btn-primary w-100" @click="close">Okay</button>
